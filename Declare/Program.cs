296d4c075b053363ceb9dc35f48b9b38fb8e3aac
Du Luong Khanh <du.luongkhanh@nashtechglobal.com>
2025-01-14 13:33:21 +0700

    declare variable and print out the console

---
+++ Declare/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int quantity = 1;
+        double price = 12.25;
+        string description = "Best quality...";
+        bool available = true;
+        
+        Console.WriteLine("The quantity is: " + quantity);
+        Console.WriteLine("The price is: " + price);
+        Console.WriteLine("The description is: " + description);
+        Console.WriteLine("Product is available: " + (available ? "Yes" : "No"));
+    }
+}
