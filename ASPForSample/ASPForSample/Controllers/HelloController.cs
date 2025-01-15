f01801c3f3c6bfb9ee0f4b02d051b4fe58413720
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-15 09:28:35 +0700

    Add HelloController for return greeting json

---
+++ ASPForSample/ASPForSample/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPForSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Greeting()
+        {
+            var greeting = new
+            {
+                Welcome = "You",
+                Date = DateTime.Now,
+            };
+
+            return Ok(Newtonsoft.Json.JsonConvert.SerializeObject(greeting));
+        }
+
+    }
+}
