c1dd1352593531ef024fb7a3472a8ebd9e8b8159
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-15 10:59:36 +0700

    Newtonsoft Json

---
+++ NewtonJson/NewtonJson/Program.cs
@@ -0,0 +1,15 @@
+﻿public class NewtonJson
+{
+    public static void Main(string[] args)
+    {
+        var person = new
+        {
+            Name = "John",
+            Age = 30
+        };
+
+        var personJson = Newtonsoft.Json.JsonConvert.SerializeObject(person);
+
+        Console.WriteLine(personJson);
+    }
+}
