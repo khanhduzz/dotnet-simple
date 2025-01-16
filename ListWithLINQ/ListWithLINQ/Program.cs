5b0683bb4bd1e07955d2cbf62b923e65cfc7ab3e
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-16 13:15:13 +0700

    List with LINQ query

---
+++ ListWithLINQ/ListWithLINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿public class ListWithLINQ
+{
+    public static void Main(string[] args)
+    {
+        List<int> input = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+        Console.Write("The original list: ");
+        foreach (int i in input) Console.Write(i + " ");
+
+        var result = from i in input
+                          where i % 2 == 0
+                          orderby i descending
+                          select i;
+
+        Console.Write("\nFiltered and sorted list: ");
+        foreach (int i in result) Console.Write(i + " ");
+    }
+}
