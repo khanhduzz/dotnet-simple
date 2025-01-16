1094ff63faa52814f1c81092bdb1e06611070873
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-16 10:27:32 +0700

    Create a list, then remove element, print out the console

---
+++ BasicList/BasicList/Program.cs
@@ -0,0 +1,24 @@
+﻿public class BasicList
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("The list from 1 to 5: ");
+
+        printList(numbers);
+
+        numbers.Remove(3);
+
+        Console.WriteLine("The list after removed: ");
+        printList(numbers);
+    }
+
+    public static void printList(List<int> list)
+    {
+        foreach (int i in list)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
