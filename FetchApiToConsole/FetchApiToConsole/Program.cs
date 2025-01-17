a166a6aa0c6cb17eab92fc69da3bee3e9bc01db8
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-17 09:46:12 +0700

    Fetch api and print console

---
+++ FetchApiToConsole/FetchApiToConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿public class FetchJson
+{
+    static async Task Main(string[] args)
+    {
+        using HttpClient client = new HttpClient();
+        string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        Console.WriteLine("Fetching data...");
+        string response = await client.GetStringAsync(url);
+
+        Console.WriteLine("Response:");
+        Console.WriteLine(response);
+    }
+}   
