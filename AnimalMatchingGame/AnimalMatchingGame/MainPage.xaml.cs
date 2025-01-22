dfce8c8afadc2a1c4ee8ef69188bd4de058424d6
Khanh Du <luongkhanhdu95@gmail.com>
2025-01-22 16:08:48 +0700

    Animal Matching Game

---
+++ AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+            List<string> animalEmoji = [
+                "🐯","🐯",
+                "🦒","🦒",
+                "🐼","🐼",
+                "🐖","🐖",
+                "🐋","🐋",
+                "🐟","🐟",
+                "🐓","🐓",
+                "🐧","🐧",
+            ];
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
